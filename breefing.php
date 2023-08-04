548966808971de05dae8e5744b617d26abc82f5e
Fauzi Agustian <33486013+zifaucode@users.noreply.github.com>
2023-08-04 16:23:37 +0700

    Update breefing.php

--- breefing.php
+++ breefing.php
@@ -3,42 +3,21 @@
 Route::get('/', function() {
     $students = [
         [
-            'first_name' => 'Rara',
-            'last_name' => 'Anggara',
-            'grade' => '1-A',
-            'age' => 10,
-            'spp' => 2000, 
-        ],
-        [
-            'first_name' => 'Faisal',
-            'last_name' => 'Reza',
+            'first_name' => 'Fauzi',
+            'last_name' => 'Keren',
             'grade' => '1-B',
             'age' => 10,
             'spp' => 4000, 
         ],
         [
             'first_name' => 'Fauzi',
-            'last_name' => 'Agustin',
+            'last_name' => 'Agustian',
             'grade' => '2-C',
             'age' => 9,
             'spp' => 6000, 
-        ],
-        [   
-            'first_name' => 'Airwan',
-            'last_name' => 'H',
-            'grade' => '2-D',
-            'age' => 5,
-            'spp' => 1500, 
-        ],
-        [
-            'first_name' => 'Rezha',
-            'last_name' => 'Pahlevi',
-            'grade' => '3-D',
-            'age' => 12,
-            'spp' => 7000, 
-        ],
-    ];
+        ];
 
+    // ================================ NO 1 ===============================
     // $student = collect($students)->map(function($student) {
     //   return [ 
     //       'name' => $student['first_name'],
@@ -47,44 +26,32 @@ Route::get('/', function() {
     // })->all();
 
     // return $student;
+    // ================================ NO 1 ===============================
 
+
+    // ================================ NO 2 ===============================
     // $student = collect($students)->groupBy(function($studentNewKey){
     //    return substr($studentNewKey['grade'], 0, 1);
     //     // return [$grade => $studentNewKey];
     // })->all();
     // return $student;
 
-    // $replaceKey = collect($student)->map(function ($studentNewKey, $key) {
-    //     // $grade = substr($key, strpos($key, '-') + 1);
-    //     $grade = substr($key, 0, 1);
-    //     // $numberGrade = (float) $grade;
-    //     // $newKey = $numberGrade;
-
-    //     return [$grade => $studentNewKey];
-    // })->all();
+    // ================================ NO 2 ===============================
 
 
-
-
-    $student = collect($students)->filter(function($student){
-      return $student['age']  % 2 != 0;
+    // ================================ NO 3 ===============================
+    // $student = collect($students)->filter(function($student){
+    //   return $student['age']  % 2 != 0;
       
-    })->values()
-    ->map(function($stdn){
-      return collect($stdn)->only(['first_name', 'age']);
-    })
-    ->all();
+    // })->values()
+    // ->map(function($stdn){
+    //   return collect($stdn)->only(['first_name', 'age']);
+    // })
+    // ->all();
     // })->only(['first_name', 'age'])->all();
-
-  // $filtered = collect($student)->map(function($studentFiltered){
-  //   return [
-  //     'name' => $studentFiltered['first_name'],
-  //     'age' => $studentFiltered['age'],
-  //   ];
-
-  // })->all();
-
-    return $student;
+   
+    // return $student;
+     // ================================ NO 3 ===============================
   
 
     
@@ -93,22 +60,22 @@ Route::get('/', function() {
     // Latihan
     // 1. Ubah menjadi struktur ini menggunakan map:
     // [
-    //     'name' => 'Rara Anggara',
+    //     'name' => 'Fauzi Agustian',
     //     'age_spp' => '10_4000', 
     // ]
 
     // 2. Kelompokkan berdasarkan kelas menjadi struktur ini menggunakan groupBy:
     // [
     //     '1' => [
-    //         'first_name' => 'Rara',
-    //         'last_name' => 'Anggara',
+    //         'first_name' => 'Fauzi',
+    //         'last_name' => 'Agustian',
     //         'grade' => '1-A',
     //         'age' => 10,
     //         'spp' => 2000, 
     //     ],
     //     [
-    //         'first_name' => 'Faisal',
-    //         'last_name' => 'Reza',
+    //         'first_name' => 'Fauzi',
+    //         'last_name' => 'Keren',
     //         'grade' => '1-B',
     //         'age' => 10,
     //         'spp' => 4000, 
